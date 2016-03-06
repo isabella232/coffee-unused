a22ff3901e3f781ba38f263a13b8b03d80fa4063
Hakan Karadis <hkaradi1@binghamton.edu>
2016-03-06 13:42:45 -0800

    command line arguments are handled well and read me edited

--- Readme.md
+++ Readme.md
@@ -12,5 +12,9 @@ npm install coffee-unused
 ## Usage
 
 ```
-node ./node_modules/index.js <filename.coffee>
+node ./node_modules/index.js --src <path to walk> [--unused-vars or --unused-reqs]
+
+-v alias for '--unused-vars'
+-r alias for '--unused-reqs'
+
 ```
--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-require('coffee-script/register')
-var vars = require('./vars')
-var reqs = require('./find-unused-reqs')
+require('coffee-script/register');
+var vars = require('./vars');
+var reqs = require('./find-unused-reqs');
+var commandLineArgs = require('command-line-args');
 
-if (process.argv.length < 3) {
-  console.log('Usage: analyze.coffee file.coffee');
-  process.exit(1)
+var cli = commandLineArgs([
+  { name: 'unused-vars', alias: 'v', type: Boolean },
+  { name: 'src', type: String},
+  { name: 'unused-reqs', alias: 'r', type: Boolean }
+])
+
+var options = cli.parse()
+
+if (options['unused-vars'] && options['unused-reqs'])
+  usage();
+
+if (!options.src)
+  usage();
+
+if (typeof options.src === 'string') {
+  if (options['unused-vars']) {
+    vars(options.src);
+  }
+  if (options['unused-reqs']) {
+    reqs(options.src)
+  }
 }
 
-vars(process.argv[2])
-//reqs(process.argv[2])
+function usage(){
+  console.log('usage: node index.js --src <path to walk> [--unused-vars or --unused-reqs]');
+  process.exit(1);
+}
