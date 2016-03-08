a351b238ace61aed970f7c6c721be34bad465520
Hakan Karadis <hkaradi1@binghamton.edu>
2016-03-08 12:19:37 -0800

    added skip-parse-error flag

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var commandLineArgs = require('command-line-args');
 var cli = commandLineArgs([
   { name: 'unused-vars', alias: 'v', type: Boolean },
   { name: 'src', type: String},
-  { name: 'unused-reqs', alias: 'r', type: Boolean }
+  { name: 'unused-reqs', alias: 'r', type: Boolean },
+  { name: 'skip-parse-error', alias: 's', type: Boolean}
 ])
 
 var options = cli.parse()
@@ -19,7 +20,7 @@ if (!options.src)
 
 if (typeof options.src === 'string') {
   if (options['unused-vars']) {
-    vars(options.src);
+    vars(options.src, options['skip-parse-error']);
   }
   if (options['unused-reqs']) {
     reqs(options.src)
